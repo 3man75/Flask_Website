165f893c26f3d092024802b4cf88c028dbb92c86
Javier <Cadetmunoz@gmail.com>
2022-05-24 17:34:40 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,37 @@
+import shutil
+from flask import Flask
+from flask_bootstrap import Bootstrap
+from flask import render_template
+import requests
+import json
+import urllib
+import api
+
+
+
+app = Flask(__name__)
+Bootstrap(app)
+@app.route('/')
+def create_app():
+
+    print(app)
+
+    return returned_dict
+
+@app.route('/dog')
+def doggies():
+
+    #calls the function in the api file
+    doggy= api.get_dog_images()
+
+    return render_template("dog.html", dog=doggy)
+
+if __name__ == '__main__':
+
+    #Calls JSON_Data and sends it to the initial flask page from api file
+    returned_dict = api.get_dogs()
+
+    #Creates first website
+    create_app()
+
+    app.run(host="0.0.0.0", debug=True, port=2000)
